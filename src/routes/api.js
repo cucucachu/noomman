0aff107b1ec4e1f1ce7d5a564ff416163fabdf36
Cody Jones <codymjones@ymail.com>
2019-12-18 11:20:23 -0800

    Removed all public square and express code.

--- src/routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/tokenTest', function(request, response, next) {
-	response.json(request.user);
-});
-
-module.exports = router;
